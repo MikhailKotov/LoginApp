40b01ea1691a7356c20f13a9e8312642cfa73a52
Mykhailo Kotov <mkhlktv@gmail.com>
2025-04-05 12:00:20 +0200

    Initial Commit

---
+++ LoginApp/LoginAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginAppApp.swift
+//  LoginApp
+//
+//  Created by Mykhailo Kotov on 05/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoginAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
